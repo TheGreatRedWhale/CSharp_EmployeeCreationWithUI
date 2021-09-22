3077b6a998362f502e7493c7ad1a7efa1f4442e5
Jared Whaley <jjw0008@auburn.edu>
2021-09-22 14:49:17 -0400

    - Created .Data section of project. - Created Employee Abstract Class.     - Added Name property     - Added Number property.

---
+++ ProductionWorkerManager.Data/Employee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductionWorkerManager.Data
+{
+    public abstract class Employee
+    {
+        private string _name;
+        private int _number;
+
+        public virtual string Name
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+
+        public virtual int Number
+        {
+            get
+            {
+                return _number;
+            }
+            set
+            {
+                _number = value;
+            }
+        }
+    }
+}
