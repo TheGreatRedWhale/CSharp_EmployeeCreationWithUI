ceb2e6ffb53da1d57ea9853bfc5e980ea1098965
Jared Whaley <jjw0008@auburn.edu>
2021-09-22 16:36:13 -0400

    - Corrected some minor logic errors within the code.

--- ProductionWorkerManager.UI/MainForm.cs
+++ ProductionWorkerManager.UI/MainForm.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,35 +26,35 @@ namespace ProductionWorkerManager.UI
             ProductionWorker employee = new ProductionWorker();
 
             // Get the employee's name
-            employee.Name = inputEmployeeNameLabel.Text;
+            employee.Name = inputEmployeeNameTextBox.Text;
 
             // Get the employee's shift number
-            try
+            if (int.TryParse(inputEmployeeShiftTextBox.Text, out int shiftNumber))
             {
-                if (int.TryParse(inputEmployeeShiftTextBox.Text, out int shiftNumber))
+                if (Enum.IsDefined(typeof(ProductionWorker.Shift), shiftNumber))
                 {
-                    employee.ShiftNumber = (ProductionWorker.Shift)int.Parse(inputEmployeeShiftTextBox.Text);
-                }
-                else if (inputEmployeeShiftTextBox.Text.ToUpper().Equals("DAY") ||
-                    inputEmployeeShiftTextBox.Text.ToUpper().Equals("D") ||
-                    inputEmployeeShiftTextBox.Text.ToUpper().Equals("MORNING") ||
-                    inputEmployeeShiftTextBox.Text.ToUpper().Equals("M"))
-                {
-                    employee.ShiftNumber = ProductionWorker.Shift.Day;
-                }
-                else if (inputEmployeeShiftTextBox.Text.ToUpper().Equals("NIGHT") ||
-                    inputEmployeeShiftTextBox.Text.ToUpper().Equals("N") ||
-                    inputEmployeeShiftTextBox.Text.ToUpper().Equals("EVENING") ||
-                    inputEmployeeShiftTextBox.Text.ToUpper().Equals("E"))
-                {
-                    employee.ShiftNumber = ProductionWorker.Shift.Night;
+                    employee.ShiftNumber = (ProductionWorker.Shift)shiftNumber;
                 }
                 else
                 {
                     employee.ShiftNumber = ProductionWorker.Shift.Unassigned;
                 }
             }
-            catch (Exception)
+            else if (inputEmployeeShiftTextBox.Text.ToUpper().Equals("DAY") ||
+                inputEmployeeShiftTextBox.Text.ToUpper().Equals("D") ||
+                inputEmployeeShiftTextBox.Text.ToUpper().Equals("MORNING") ||
+                inputEmployeeShiftTextBox.Text.ToUpper().Equals("M"))
+            {
+                employee.ShiftNumber = ProductionWorker.Shift.Day;
+            }
+            else if (inputEmployeeShiftTextBox.Text.ToUpper().Equals("NIGHT") ||
+                inputEmployeeShiftTextBox.Text.ToUpper().Equals("N") ||
+                inputEmployeeShiftTextBox.Text.ToUpper().Equals("EVENING") ||
+                inputEmployeeShiftTextBox.Text.ToUpper().Equals("E"))
+            {
+                employee.ShiftNumber = ProductionWorker.Shift.Night;
+            }
+            else
             {
                 employee.ShiftNumber = ProductionWorker.Shift.Unassigned;
             }
@@ -85,9 +86,16 @@ namespace ProductionWorkerManager.UI
         public void DisplayProductionWorker(ProductionWorker productionWorker)
         {
             outputEmployeeNameTextBox.Text = productionWorker.Name;
-            outputEmployeeNumberTextBox.Text = productionWorker.Number.ToString();
+            if (productionWorker.Number == -1.0)
+            {
+                outputEmployeeNumberTextBox.Text = "Unassigned";
+            }
+            else
+            {
+                outputEmployeeNumberTextBox.Text = productionWorker.Number.ToString();
+            }
             outputEmployeeShiftTextBox.Text = productionWorker.ShiftNumber.ToString();
-            outputEmployeeHourlyRateTextBox.Text = productionWorker.HourlyRate.ToString();
+            outputEmployeeHourlyRateTextBox.Text = productionWorker.HourlyRate.ToString("C2", CultureInfo.CurrentCulture);
         }
 
         public void ClearInput()
@@ -102,8 +110,8 @@ namespace ProductionWorkerManager.UI
 
         private void createObjectButton_Click(object sender, EventArgs e)
         {
-            ProductionWorker employee = CreateProductionWorker();
-
+            ProductionWorker productionWorker = CreateProductionWorker();
+            DisplayProductionWorker(productionWorker);
         }
     }
 }
